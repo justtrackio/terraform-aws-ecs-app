5af5c173c338600dff54aa1c08476a3ed7fb24ae
Marco Frömbgen <23717573+mfroembgen@users.noreply.github.com>
2023-05-10 18:58:52 +0200

    feat: add_common_capabilities

--- ecr.tf
+++ ecr.tf
@@ -1,14 +1,50 @@
-module "ecr" {
-  source  = "cloudposse/ecr/aws"
-  version = "0.35.0"
+module "ecr_label" {
+  source  = "cloudposse/label/null"
+  version = "0.25.0"
 
   label_order = var.label_orders.ecr
   delimiter   = "/"
 
-  image_tag_mutability = "MUTABLE"
-  max_image_count      = 20
-  scan_images_on_push  = false
-  force_delete         = true
-
   context = module.this.context
+}
+
+module "ecr" {
+  source  = "terraform-aws-modules/ecr/aws"
+  version = "1.6.0"
+
+  repository_name = module.ecr_label.id
+  tags            = module.ecr_label.tags
+
+  attach_repository_policy        = false
+  repository_image_scan_on_push   = false
+  repository_image_tag_mutability = "MUTABLE"
+  repository_force_delete         = true
+  repository_lifecycle_policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1,
+        description  = "Remove untagged images",
+        selection = {
+          countNumber = 1
+          countType   = "imageCountMoreThan"
+          tagStatus   = "untagged"
+        },
+        action = {
+          type = "expire"
+        }
+      },
+      {
+        rulePriority = 2,
+        description  = "Keep last 30 images",
+        selection = {
+          tagStatus   = "any",
+          countType   = "imageCountMoreThan",
+          countNumber = 30
+        },
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
 }
