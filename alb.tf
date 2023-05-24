748a5ce5396b046ce518fb7a9c3779ea4f883777
Marco Frömbgen <23717573+mfroembgen@users.noreply.github.com>
2023-05-24 11:50:07 +0200

    feat: add_common_capabilities

--- alb.tf
+++
@@ -1,9 +0,0 @@
-#module "alb_ingress" {
-#  source  = "cloudposse/alb-ingress/aws"
-#  version = "0.25.1"
-#
-#  label_key_case = ["namespace", "stage"]
-#
-#  context = module.this.context
-#  vpc_id  = var.vpc_id
-#}
