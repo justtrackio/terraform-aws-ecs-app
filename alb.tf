210163c4d9e3c302190a7b66ee91c71d15a3b16a
Marco Frömbgen <23717573+mfroembgen@users.noreply.github.com>
2023-07-25 17:05:00 +0200

    feat!: add_common_capabilities

--- alb.tf
+++ alb.tf
@@ -14,6 +14,7 @@ module "alb_ingress" {
   unauthenticated_hosts         = var.alb_unauthenticated_hosts
   unauthenticated_listener_arns = [data.aws_lb_listener.http[0].arn, data.aws_lb_listener.https[0].arn]
 
-  health_check_path    = "/health"
-  health_check_matcher = "200"
+  health_check_path     = "/health"
+  health_check_matcher  = "200"
+  health_check_interval = 60
 }
