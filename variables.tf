056ca9c45af53b53f2232230184633eab7961115
Sebastian Struß <struss@justtrack.io>
2023-07-28 08:02:22 +0200

    feat: add tag propagation

--- variables.tf
+++ variables.tf
@@ -491,6 +491,12 @@ variable "ordered_placement_strategy" {
     EOT
 }
 
+variable "propagate_tags" {
+  type        = string
+  description = "Specifies whether to propagate the tags from the task definition or the service to the tasks. The valid values are SERVICE and TASK_DEFINITION"
+  default     = null
+}
+
 variable "port_gateway" {
   type        = number
   description = "Define the gateway port"
