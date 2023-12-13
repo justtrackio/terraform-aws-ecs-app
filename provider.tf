12639349dd19702fb4cc25b7044faf8eb0ee988a
Marco Frömbgen <23717573+mfroembgen@users.noreply.github.com>
2023-12-13 17:13:42 +0100

    fix: change sentry to https (#76)

--- provider.tf
+++ provider.tf
@@ -25,6 +25,6 @@ provider "elasticstack" {
 }
 
 provider "sentry" {
-  base_url = "http://sentry.${module.this.organizational_unit}-monitoring.${var.domain}"
+  base_url = "https://sentry.${module.this.organizational_unit}-monitoring.${var.domain}"
   token    = data.aws_ssm_parameter.sentry_token.value
 }
