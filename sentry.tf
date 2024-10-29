e59f84dd98218dbf1d27b42e2f671431823cb2e9
Bünyamin <19335284+ztzxt@users.noreply.github.com>
2024-10-29 14:08:54 +0100

    fix: bump sentry version (#145)
    Signed-off-by: Bünyamin Dökmetaş <19335284+ztzxt@users.noreply.github.com>

--- sentry.tf
+++ sentry.tf
@@ -30,7 +30,7 @@ locals {
 module "sentry" {
   count   = module.this.enabled && var.sentry_enabled ? 1 : 0
   source  = "justtrackio/project/sentry"
-  version = "1.4.1"
+  version = "1.4.2"
 
   context     = module.this.context
   label_order = var.label_orders.sentry
