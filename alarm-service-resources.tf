82fd251ec56ec4b8a653a4f658931469392d5b3d
Marco Frömbgen <23717573+mfroembgen@users.noreply.github.com>
2024-11-19 17:57:37 +0100

    feat: add alarm levels

--- alarm-service-resources.tf
+++ alarm-service-resources.tf
@@ -2,11 +2,13 @@ module "alarm_service_resources" {
   count = var.alarm_service_resources_enabled ? 1 : 0
 
   source  = "justtrackio/alarm-service-resources/aws"
-  version = "1.1.0"
+  version = "1.2.0"
 
   context      = module.this.context
   label_orders = var.label_orders
 
+  alarm_description  = merge(module.this.tags, module.this.additional_tag_map)
+  alarm_priority     = var.alarm_service_resources_priority
   cluster_name       = data.aws_ecs_cluster.default.cluster_name
   cpu_average        = var.alarm_service_resources_cpu_average
   cpu_maximum        = var.alarm_service_resources_cpu_maximum
