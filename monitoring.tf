d687548ff4f88bc772cccfd70503be1e2880cbf0
Marco Frömbgen <23717573+mfroembgen@users.noreply.github.com>
2024-11-22 13:12:10 +0100

    fix: update gosoline monitoring module

--- monitoring.tf
+++ monitoring.tf
@@ -12,7 +12,7 @@ locals {
 module "monitoring" {
   count   = var.monitoring_enabled ? 1 : 0
   source  = "justtrackio/ecs-gosoline-monitoring/aws"
-  version = "2.6.0"
+  version = "2.6.1"
 
   context = module.this.context
 
